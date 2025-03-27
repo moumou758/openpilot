2e0856f1599b495147194a92fbc34f4dc98c9e3f
moumou758 <moumou758@163.com>
2025-03-27 22:42:49 +0800

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 export ATHENA_HOST='ws://laolang.duckdns.org:7899'
 export API_HOST='http://laolang.duckdns.org:7898'
-export MAPBOX_TOKEN='pk.eyJ1IjoibXJvbmVjYyIsImEiOiJjbHhqbzlkbTYxNXUwMmtzZjdoMGtrZnVvIn0.SC7GNLtMFUGDgC2bAZcKzg'
+export MAPBOX_TOKEN='pk.eyJ1Ijoiam5ld2IiLCJhIjoiY2xxNW8zZXprMGw1ZzJwbzZneHd2NHljbSJ9.gV7VPRfbXFetD-1OVF0XZg'
 export SKIP_FW_QUERY=1
 exec ./launch_chffrplus.sh
