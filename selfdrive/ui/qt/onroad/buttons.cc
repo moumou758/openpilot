04ee5b4bbca55b2aafd0d7c20a82f6d992b377c9
moumou758 <moumou758@163.com>
2025-05-08 22:43:15 +0800

    Update buttons.cc

--- selfdrive/ui/qt/onroad/buttons.cc
+++ selfdrive/ui/qt/onroad/buttons.cc
@@ -4,19 +4,26 @@
 
 #include "selfdrive/ui/qt/util.h"
 
-void drawIcon(QPainter &p, const QPoint &center, const QPixmap &img, const QBrush &bg, float opacity) {
+// 修改后的 drawIcon，支持传入 angle 并旋转图标
+void drawIcon(QPainter &p, const QPoint &center, const QPixmap &img, const QBrush &bg, float opacity, int angle = 0) {
   p.setRenderHint(QPainter::Antialiasing);
-  p.setOpacity(0.6);  // bg dictates opacity of ellipse
+  p.setOpacity(1.0);  // bg dictates opacity of ellipse
   p.setPen(Qt::NoPen);
   p.setBrush(bg);
   p.drawEllipse(center, btn_size / 2, btn_size / 2);
+
+  p.save();
+  p.translate(center);
+  p.rotate(angle);
   p.setOpacity(opacity);
-  p.drawPixmap(center - QPoint(img.width() / 2, img.height() / 2), img);
-  p.setOpacity(0.6);
+  p.drawPixmap(-QPoint(img.width() / 2, img.height() / 2), img);
+  p.setOpacity(1.0);
+  p.restore();
 }
 
 // ExperimentalButton
-ExperimentalButton::ExperimentalButton(QWidget *parent) : experimental_mode(false), engageable(false), QPushButton(parent) {
+ExperimentalButton::ExperimentalButton(QWidget *parent)
+    : experimental_mode(false), engageable(false), steering_angle_deg(0), QPushButton(parent) {
   setFixedSize(btn_size, btn_size);
 
   engage_img = loadPixmap("../assets/img_chffr_wheel.png", {img_size, img_size});
@@ -26,7 +33,8 @@ ExperimentalButton::ExperimentalButton(QWidget *parent) : experimental_mode(fals
 
 void ExperimentalButton::changeMode() {
   const auto cp = (*uiState()->sm)["carParams"].getCarParams();
-  bool can_change = hasLongitudinalControl(cp) && params.getBool("ExperimentalModeConfirmed");
+  bool can_change = (hasLongitudinalControl(cp) || (!cp.getPcmCruiseSpeed() && params.getBool("CustomStockLongPlanner")))
+                    && params.getBool("ExperimentalModeConfirmed");
   if (can_change) {
     params.putBool("ExperimentalMode", !experimental_mode);
   }
@@ -35,15 +43,20 @@ void ExperimentalButton::changeMode() {
 void ExperimentalButton::updateState(const UIState &s) {
   const auto cs = (*s.sm)["controlsState"].getControlsState();
   bool eng = cs.getEngageable() || cs.getEnabled();
-  if ((cs.getExperimentalMode() != experimental_mode) || (eng != engageable)) {
+  int new_steering_angle = s.scene.steering_angle_deg;  // 注意这里是你车辆状态里同步的方向盘角度
+  if ((cs.getExperimentalMode() != experimental_mode) || (eng != engageable) ||
+      (steering_angle_deg != new_steering_angle)) {
     engageable = eng;
     experimental_mode = cs.getExperimentalMode();
+    steering_angle_deg = new_steering_angle;
     update();
   }
 }
 
+// 注意这里 paintEvent 传入角度
 void ExperimentalButton::paintEvent(QPaintEvent *event) {
   QPainter p(this);
   QPixmap img = experimental_mode ? experimental_img : engage_img;
-  drawIcon(p, QPoint(btn_size / 2, btn_size / 2), img, QColor(0, 0, 0, 166), (isDown() || !engageable) ? 0.5 : 0.6);
+  drawIcon(p, QPoint(btn_size / 2, btn_size / 2), img, QColor(0, 0, 0, 166),
+           (isDown() || !engageable) ? 0.6 : 1.0, steering_angle_deg);
 }
