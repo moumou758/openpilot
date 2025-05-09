2d40efca5dae774a151c35dfdba579ef84642f77
moumou758 <moumou758@163.com>
2025-05-09 16:09:17 +0800

    Update buttons.cc

--- selfdrive/ui/qt/onroad/buttons.cc
+++ selfdrive/ui/qt/onroad/buttons.cc
@@ -4,19 +4,28 @@
 
 #include "selfdrive/ui/qt/util.h"
 
-void drawIcon(QPainter &p, const QPoint &center, const QPixmap &img, const QBrush &bg, float opacity) {
+void drawIcon(QPainter &p, const QPoint &center, const QPixmap &img, const QBrush &bg, float opacity, float rotate_deg = 0.0f) {
   p.setRenderHint(QPainter::Antialiasing);
   p.setOpacity(0.6);  // bg dictates opacity of ellipse
   p.setPen(Qt::NoPen);
   p.setBrush(bg);
   p.drawEllipse(center, btn_size / 2, btn_size / 2);
   p.setOpacity(opacity);
-  p.drawPixmap(center - QPoint(img.width() / 2, img.height() / 2), img);
+
+  // 图标旋转
+  p.save();
+  p.translate(center);
+  p.rotate(rotate_deg);
+  p.drawPixmap(QPoint(-img.width() / 2, -img.height() / 2), img);
+  p.restore();
+
   p.setOpacity(0.6);
 }
 
 // ExperimentalButton
-ExperimentalButton::ExperimentalButton(QWidget *parent) : experimental_mode(false), engageable(false), QPushButton(parent) {
+ExperimentalButton::ExperimentalButton(QWidget *parent) : 
+  experimental_mode(false), engageable(false), steeringAngleDeg(0.0f), QPushButton(parent)
+{
   setFixedSize(btn_size, btn_size);
 
   engage_img = loadPixmap("../assets/img_chffr_wheel.png", {img_size, img_size});
@@ -36,6 +45,13 @@ void ExperimentalButton::changeMode() {
 void ExperimentalButton::updateState(const UIState &s) {
   const auto cs = (*s.sm)["controlsState"].getControlsState();
   bool eng = cs.getEngageable() || cs.getEnabled();
+
+  // 获取方向盘角度
+  steeringAngleDeg = 0.0f;
+  if (s.scene) {
+    steeringAngleDeg = s.scene->steering_angle_deg;
+  }
+
   if ((cs.getExperimentalMode() != experimental_mode) || (eng != engageable)) {
     engageable = eng;
     experimental_mode = cs.getExperimentalMode();
@@ -46,5 +62,5 @@ void ExperimentalButton::updateState(const UIState &s) {
 void ExperimentalButton::paintEvent(QPaintEvent *event) {
   QPainter p(this);
   QPixmap img = experimental_mode ? experimental_img : engage_img;
-  drawIcon(p, QPoint(btn_size / 2, btn_size / 2), img, QColor(0, 0, 0, 166), (isDown() || !engageable) ? 0.5 : 0.6);
+  drawIcon(p, QPoint(btn_size / 2, btn_size / 2), img, QColor(0, 0, 0, 166), (isDown() || !engageable) ? 0.5 : 0.6, steeringAngleDeg);
 }
