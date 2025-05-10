9ca695aef836fb5457259cdb567462ab04c19828
moumou758 <moumou758@163.com>
2025-05-10 14:33:05 +0800

    Update buttons.cc

--- selfdrive/ui/qt/onroad/buttons.cc
+++ selfdrive/ui/qt/onroad/buttons.cc
@@ -4,19 +4,15 @@
 
 #include "selfdrive/ui/qt/util.h"
 
-void drawIcon(QPainter &p, const QPoint &center, const QPixmap &img, const QBrush &bg, float opacity, const int angle) {
+void drawIcon(QPainter &p, const QPoint &center, const QPixmap &img, const QBrush &bg, float opacity) {
   p.setRenderHint(QPainter::Antialiasing);
   p.setOpacity(0.6);  // bg dictates opacity of ellipse
   p.setPen(Qt::NoPen);
   p.setBrush(bg);
   p.drawEllipse(center, btn_size / 2, btn_size / 2);
-  p.save();
-  p.translate(center);
-  p.rotate(angle);
   p.setOpacity(opacity);
-  p.drawPixmap(-QPoint(img.width() / 2, img.height() / 2), img);
+  p.drawPixmap(center - QPoint(img.width() / 2, img.height() / 2), img);
   p.setOpacity(0.6);
-  p.restore();
 }
 
 // ExperimentalButton
@@ -45,26 +41,10 @@ void ExperimentalButton::updateState(const UIState &s) {
     experimental_mode = cs.getExperimentalMode();
     update();
   }
-
-  const UIScene &scene = s.scene;
-
-  rotating_wheel = scene.rotating_wheel;
-
-  if (rotating_wheel && steering_angle_deg != scene.steering_angle_deg) {
-    steering_angle_deg = scene.steering_angle_deg;
-    update();
-  } else if (!rotating_wheel) {
-    steering_angle_deg = 0;
-  }
-
-  if (params_memory.getBool("UpdateWheelImage")) {
-    updateIcon();
-    params_memory.remove("UpdateWheelImage");
-  }
 }
 
 void ExperimentalButton::paintEvent(QPaintEvent *event) {
   QPainter p(this);
   QPixmap img = experimental_mode ? experimental_img : engage_img;
-  drawIcon(p, QPoint(btn_size / 2, btn_size / 2), img, QColor(0, 0, 0, 166), (isDown() || !engageable) ? 0.5 : 0.6, steering_angle_deg);
+  drawIcon(p, QPoint(btn_size / 2, btn_size / 2), img, QColor(0, 0, 0, 166), (isDown() || !engageable) ? 0.5 : 0.6);
 }
