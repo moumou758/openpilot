91a8b59a03aba6074fd0a0dd6e2f543dc9b0d4ea
moumou758 <moumou758@163.com>
2025-05-10 14:24:00 +0800

    Update buttons.h

--- selfdrive/ui/qt/onroad/buttons.h
+++ selfdrive/ui/qt/onroad/buttons.h
@@ -1,38 +1,70 @@
-#pragma once
+#include "selfdrive/ui/qt/onroad/buttons.h"
 
-#include <QPushButton>
+#include <QPainter>
 
-#ifdef SUNNYPILOT
-#include "selfdrive/ui/sunnypilot/ui.h"
-#else
-#include "selfdrive/ui/ui.h"
-#endif
+#include "selfdrive/ui/qt/util.h"
 
-const int btn_size = 192;
-const int img_size = (btn_size / 4) * 3;
+void drawIcon(QPainter &p, const QPoint &center, const QPixmap &img, const QBrush &bg, float opacity, const int angle) {
+  p.setRenderHint(QPainter::Antialiasing);
+  p.setOpacity(0.6);  // bg dictates opacity of ellipse
+  p.setPen(Qt::NoPen);
+  p.setBrush(bg);
+  p.drawEllipse(center, btn_size / 2, btn_size / 2);
+  p.save();
+  p.translate(center);
+  p.rotate(angle);
+  p.setOpacity(opacity);
+  p.drawPixmap(-QPoint(img.width() / 2, img.height() / 2), img);
+  p.setOpacity(0.6);
+  p.restore();
+}
 
-class ExperimentalButton : public QPushButton {
-  Q_OBJECT
+// ExperimentalButton
+ExperimentalButton::ExperimentalButton(QWidget *parent) : experimental_mode(false), engageable(false), QPushButton(parent) {
+  setFixedSize(btn_size, btn_size);
 
-public:
-  explicit ExperimentalButton(QWidget *parent = 0);
-  void updateState(const UIState &s);
+  engage_img = loadPixmap("../assets/img_chffr_wheel.png", {img_size, img_size});
+  experimental_img = loadPixmap("../assets/img_experimental.svg", {img_size, img_size});
+  QObject::connect(this, &QPushButton::clicked, this, &ExperimentalButton::changeMode);
+}
 
-protected:
-  bool experimental_mode;
-  bool engageable;
+void ExperimentalButton::changeMode() {
+  const auto cp = (*uiState()->sm)["carParams"].getCarParams();
+  bool can_change = (hasLongitudinalControl(cp) || (!cp.getPcmCruiseSpeed() && params.getBool("CustomStockLongPlanner")))
+                    && params.getBool("ExperimentalModeConfirmed");
+  if (can_change) {
+    params.putBool("ExperimentalMode", !experimental_mode);
+  }
+}
 
-private:
-  void paintEvent(QPaintEvent *event) override;
-  void changeMode();
+void ExperimentalButton::updateState(const UIState &s) {
+  const auto cs = (*s.sm)["controlsState"].getControlsState();
+  bool eng = cs.getEngageable() || cs.getEnabled();
+  if ((cs.getExperimentalMode() != experimental_mode) || (eng != engageable)) {
+    engageable = eng;
+    experimental_mode = cs.getExperimentalMode();
+    update();
+  }
 
-  Params params;
-  QPixmap engage_img;
-  QPixmap experimental_img;
+  const UIScene &scene = s.scene;
 
-  bool rotating_wheel;
+  rotating_wheel = scene.rotating_wheel;
 
-  int steering_angle_deg;
-};
+  if (rotating_wheel && steering_angle_deg != scene.steering_angle_deg) {
+    steering_angle_deg = scene.steering_angle_deg;
+    update();
+  } else if (!rotating_wheel) {
+    steering_angle_deg = 0;
+  }
 
-void drawIcon(QPainter &p, const QPoint &center, const QPixmap &img, const QBrush &bg, float opacity);
+  if (params_memory.getBool("UpdateWheelImage")) {
+    updateIcon();
+    params_memory.remove("UpdateWheelImage");
+  }
+}
+
+void ExperimentalButton::paintEvent(QPaintEvent *event) {
+  QPainter p(this);
+  QPixmap img = experimental_mode ? experimental_img : engage_img;
+  drawIcon(p, QPoint(btn_size / 2, btn_size / 2), img, QColor(0, 0, 0, 166), (isDown() || !engageable) ? 0.5 : 0.6, steering_angle_deg);
+}
