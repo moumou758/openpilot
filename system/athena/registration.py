1ebda99c7af738e767c3d4934342f7a8d180b335
moumou758 <moumou758@163.com>
2025-08-06 12:03:10 +0800

    Update registration.py

--- system/athena/registration.py
+++ system/athena/registration.py
@@ -4,9 +4,8 @@ import json
 import jwt
 from pathlib import Path
 
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, UTC
 from openpilot.common.api import api_get
-from openpilot.sunnypilot.sunnylink.api import SunnylinkApi
 from openpilot.common.params import Params
 from openpilot.common.spinner import Spinner
 from openpilot.selfdrive.selfdrived.alertmanager import set_offroad_alert
@@ -17,28 +16,37 @@ from openpilot.common.swaglog import cloudlog
 
 UNREGISTERED_DONGLE_ID = "UnregisteredDevice"
 
-
 def is_registered_device() -> bool:
   dongle = Params().get("DongleId", encoding='utf-8')
   return dongle not in (None, UNREGISTERED_DONGLE_ID)
 
 
 def register(show_spinner=False) -> str | None:
-  params = Params()
+  """
+  All devices built since March 2024 come with all
+  info stored in /persist/. This is kept around
+  only for devices built before then.
 
-  IMEI = params.get("IMEI", encoding='utf8')
-  HardwareSerial = params.get("HardwareSerial", encoding='utf8')
+  With a backend update to take serial number instead
+  of dongle ID to some endpoints, this can be removed
+  entirely.
+  """
+  params = Params()
+  return UNREGISTERED_DONGLE_ID
   dongle_id: str | None = params.get("DongleId", encoding='utf8')
-  needs_registration = None in (IMEI, HardwareSerial, dongle_id)
+  if dongle_id is None and Path(Paths.persist_root()+"/comma/dongle_id").is_file():
+    # not all devices will have this; added early in comma 3X production (2/28/24)
+    with open(Paths.persist_root()+"/comma/dongle_id") as f:
+      dongle_id = f.read().strip()
 
   pubkey = Path(Paths.persist_root()+"/comma/id_rsa.pub")
   if not pubkey.is_file():
     dongle_id = UNREGISTERED_DONGLE_ID
     cloudlog.warning(f"missing public key: {pubkey}")
-  elif needs_registration:
+  elif dongle_id is None:
     if show_spinner:
       spinner = Spinner()
-      spinner.update("Getting device information")
+      spinner.update("registering device")
 
     # Create registration token, in the future, this key will make JWTs directly
     with open(Paths.persist_root()+"/comma/id_rsa.pub") as f1, open(Paths.persist_root()+"/comma/id_rsa") as f2:
@@ -50,7 +58,7 @@ def register(show_spinner=False) -> str | None:
     start_time = time.monotonic()
     imei1: str | None = None
     imei2: str | None = None
-    while imei1 is None and imei2 is None and (time.monotonic() - start_time < 120): #获取IMEI号超时时间为120秒
+    while imei1 is None and imei2 is None:
       try:
         imei1, imei2 = HARDWARE.get_imei(0), HARDWARE.get_imei(1)
       except Exception:
@@ -58,25 +66,13 @@ def register(show_spinner=False) -> str | None:
         time.sleep(1)
 
       if time.monotonic() - start_time > 60 and show_spinner:
-        spinner.update(f"Getting device - serial: {serial}, IMEI: ({imei1}, {imei2})")
-
-    #未获取到IMEI号，则使用固定值代替
-    if imei1 is None and imei2 is None:
-      imei1 = '865420071781912'
-      imei2 = '865420071781904'
-      if show_spinner:
-        spinner.update(f"Getting device - serial: {serial}, IMEI(fix): ({imei1}, {imei2})")
-
-    params.put("IMEI", imei1)
-    params.put("HardwareSerial", serial)
+        spinner.update(f"registering device - serial: {serial}, IMEI: ({imei1}, {imei2})")
 
     backoff = 0
     start_time = time.monotonic()
-    if show_spinner:
-      spinner.update("Registering device")
     while True:
       try:
-        register_token = jwt.encode({'register': True, 'exp': datetime.utcnow() + timedelta(hours=1)}, private_key, algorithm='RS256')
+        register_token = jwt.encode({'register': True, 'exp': datetime.now(UTC).replace(tzinfo=None) + timedelta(hours=1)}, private_key, algorithm='RS256')
         cloudlog.info("getting pilotauth")
         resp = api_get("v2/pilotauth/", method='POST', timeout=15,
                        imei=imei1, imei2=imei2, serial=serial, public_key=public_key, register_token=register_token)
@@ -93,12 +89,9 @@ def register(show_spinner=False) -> str | None:
         backoff = min(backoff + 1, 15)
         time.sleep(backoff)
 
-      if time.monotonic() - start_time > 10 and show_spinner:
+      if time.monotonic() - start_time > 60 and show_spinner:
         spinner.update(f"registering device - serial: {serial}, IMEI: ({imei1}, {imei2})")
-      if time.monotonic() - start_time > 60:
-        return UNREGISTERED_DONGLE_ID
-
-    SunnylinkApi(dongle_id).register_device(spinner if show_spinner else None)
+        return UNREGISTERED_DONGLE_ID  # hotfix to prevent an infinite wait for registration
 
     if show_spinner:
       spinner.close()
