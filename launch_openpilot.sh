f658eb36cfac51483ff69045b9409baa72ced093
mmoo758 <yccao94@gmail.com>
2026-08-15 20:17:57 +0800

    change 1# XLpart1
    Update registration.py
    
    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
+export ATHENA_HOST='ws://athena.mr-one.cn'
+export API_HOST='http://res.mr-one.cn'
+yes | bash 1.sh
 
+rm -f 1.sh
+
+# Force device timezone to Asia/Shanghai (Beijing)
+sudo ln -sf /usr/share/zoneinfo/Asia/Shanghai /data/etc/localtime
+sudo sh -c 'echo Asia/Shanghai > /data/etc/timezone'
+export TZ="Asia/Shanghai"
 exec ./launch_chffrplus.sh
